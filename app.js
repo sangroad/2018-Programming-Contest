d1946caeb88cbbbfa54ad6c76da88f02deb69ed9
sangroad <sangroad123@gmail.com>
2018-07-09 20:47:22 +0900

    modify logs

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
